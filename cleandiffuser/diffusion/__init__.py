c103d02fda1ff76d72419e1280e83d331115fb9e
YifuYuan <yuanyf@tju.edu.cn>
2024-06-24 16:12:19 +0800

    update

--- cleandiffuser/diffusion/__init__.py
+++ cleandiffuser/diffusion/__init__.py
@@ -2,3 +2,4 @@ from .basic import DiffusionModel
 from .diffusionsde import DiscreteDiffusionSDE, ContinuousDiffusionSDE
 from .newedm import ContinuousEDM
 from .rectifiedflow import DiscreteRectifiedFlow, ContinuousRectifiedFlow
+from .consistency_model import ContinuousConsistencyModel
