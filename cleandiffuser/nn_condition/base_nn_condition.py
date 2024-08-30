2eef1e396676571d375b2c1060f984f0afcd64bd
ZibinDong <zibindong@outlook.com>
2024-08-30 13:10:07 +0800

    update

--- cleandiffuser/nn_condition/base_nn_condition.py
+++ cleandiffuser/nn_condition/base_nn_condition.py
@@ -1,37 +1,36 @@
 import torch
 import torch.nn as nn
 
-from cleandiffuser.utils import at_least_ndim
+from cleandiffuser.utils import at_least_ndim, TensorDict
+from typing import Union
 
 
 def get_mask(mask: torch.Tensor, mask_shape: tuple, dropout: float, train: bool, device: torch.device):
     if train:
         mask = (torch.rand(mask_shape, device=device) > dropout).float()
     else:
-        mask = 1. if mask is None else mask
+        mask = 1.0 if mask is None else mask
     return mask
 
 
 class BaseNNCondition(nn.Module):
     """
-    In decision-making tasks, generating condition selections can be very diverse,
-    including cumulative rewards, languages, images, demonstrations, and so on.
-    It can even be a combination of these conditions. Therefore, we aim for
-    nn_condition to handle diverse condition selections flexibly and
-    ultimately output a tensor of shape (b, *cond_out_shape).
-
-    Input:
-        - condition: Any
-        - mask : Optional, (b, ) or None, None means no mask
-
-    Output:
-        - condition: (b, *cond_out_shape)
+    In decision-making tasks, conditions of generation can be very diverse,
+    including cumulative rewards, language instructions, images, demonstrations, and so on.
+    They can even be combinations of these conditions. Therefore, we aim for
+    `nn_condition` to handle diverse condition selections flexibly and
+    ultimately output a `torch.Tensor` or a `TensorDict`. `TensorDict` is a nested dictionary, where
+    the keys are strings and the values are `torch.Tensor` or `TensorDict`.
+    Although `TensorDict` provides much more flexibility, it must be combined with a specific `nn_diffusion` that
+    can handle the `TensorDict`. So most of our implementations only use `torch.Tensor` as conditions.
     """
 
-    def __init__(self, ):
+    def __init__(
+        self,
+    ):
         super().__init__()
 
-    def forward(self, condition: torch.Tensor, mask: torch.Tensor = None):
+    def forward(self, condition: Union[TensorDict, torch.Tensor], mask: Union[TensorDict, torch.Tensor] = None):
         raise NotImplementedError
 
 
@@ -52,6 +51,7 @@ class IdentityCondition(BaseNNCondition):
         self.dropout = dropout
 
     def forward(self, condition: torch.Tensor, mask: torch.Tensor = None):
-        mask = at_least_ndim(get_mask(
-            mask, (condition.shape[0],), self.dropout, self.training, condition.device), condition.dim())
+        mask = at_least_ndim(
+            get_mask(mask, (condition.shape[0],), self.dropout, self.training, condition.device), condition.dim()
+        )
         return condition * mask
