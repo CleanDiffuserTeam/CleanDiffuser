05f17fc9dbeae7c19a5e264632c9ae9aaac5994e
Josh00-Lu <1259754068@qq.com>
2025-04-20 22:45:29 +0800

    update for dv to improve stability

--- cleandiffuser/dataset/d4rl_kitchen_dataset.py
+++ cleandiffuser/dataset/d4rl_kitchen_dataset.py
@@ -371,34 +371,33 @@ class DV_D4RLKitchenSeqDataset(BaseDataset):
         self.o_dim, self.a_dim = observations.shape[-1], actions.shape[-1]
 
         self.indices = []
-        self.seq_obs, self.seq_next_obs, self.seq_act, self.seq_rew = [], [], [], []
+        self.seq_obs, self.seq_act, self.seq_rew = [], [], []
 
-        self.path_lengths, ptr = [], 0
+        ptr = 0
         path_idx = 0
         for i in range(timeouts.shape[0]):
             if timeouts[i] or terminals[i] or i == timeouts.shape[0] - 1:
-                self.path_lengths.append(i - ptr + 1)
+                path_length = i - ptr + 1
+                assert path_length <= max_path_length
 
-                _seq_obs = np.zeros((max_path_length, self.o_dim), dtype=np.float32)
-                _seq_next_obs = np.zeros((max_path_length, self.o_dim), dtype=np.float32)
-                _seq_act = np.zeros((max_path_length, self.a_dim), dtype=np.float32)
-                _seq_rew = np.zeros((max_path_length, 1), dtype=np.float32)
+                _seq_obs = np.zeros((max_path_length + (horizon - 1) * stride, self.o_dim), dtype=np.float32)
+                _seq_act = np.zeros((max_path_length + (horizon - 1) * stride, self.a_dim), dtype=np.float32)
+                _seq_rew = np.zeros((max_path_length + (horizon - 1) * stride, 1), dtype=np.float32)
 
-                _seq_obs[:i - ptr + 1] = normed_observations[ptr:i + 1]
-                _seq_act[:i - ptr + 1] = actions[ptr:i + 1]
-                _seq_rew[:i - ptr + 1] = rewards[ptr:i + 1][:, None]
+                _seq_obs[:path_length] = normed_observations[ptr:i + 1]
+                _seq_act[:path_length] = actions[ptr:i + 1]
+                _seq_rew[:path_length] = rewards[ptr:i + 1][:, None]
 
                 # repeat padding
-                _seq_obs[i - ptr + 1:] = normed_observations[i]  # repeat last state
-                _seq_act[i - ptr + 1:] = 0  # repeat zero action
-                _seq_rew[i - ptr + 1:] = rewards[i]  # repeat last reward
+                _seq_obs[path_length:] = normed_observations[i]  # repeat last state
+                _seq_act[path_length:] = 0
+                _seq_rew[path_length:] = rewards[i]
 
                 self.seq_obs.append(_seq_obs)
-                self.seq_next_obs.append(_seq_next_obs)
                 self.seq_act.append(_seq_act)
                 self.seq_rew.append(_seq_rew)
 
-                max_start = min(self.path_lengths[-1] - 1, max_path_length - (horizon - 1) * stride - 1)
+                max_start = path_length - 1
                 self.indices += [(path_idx, start, start + (horizon - 1) * stride + 1) for start in range(max_start + 1)]
 
                 ptr = i + 1
